62591e7d88e3d25e1bc66f99fa04853c02667417
Oriana <oriana.nohemi22@gmail.com>
2020-09-12 22:20:55 -0500

    Updates

---
+++ precache-manifest.82b212a79cfeac513210a2421efa9f1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f23d861d74887d02e0c5b2bff5bb77d",
+    "url": "/tic-tac-toe-react/index.html"
+  },
+  {
+    "revision": "db28212f63edff423d88",
+    "url": "/tic-tac-toe-react/static/css/main.71951470.chunk.css"
+  },
+  {
+    "revision": "4e0c5622e2594c5804f0",
+    "url": "/tic-tac-toe-react/static/js/2.96c522c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-react/static/js/2.96c522c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db28212f63edff423d88",
+    "url": "/tic-tac-toe-react/static/js/main.3133e1d4.chunk.js"
+  },
+  {
+    "revision": "fb8e16a8818e8da85f85",
+    "url": "/tic-tac-toe-react/static/js/runtime-main.ccc66055.js"
+  },
+  {
+    "revision": "ffb5e56351a8840b788189d90a083cef",
+    "url": "/tic-tac-toe-react/static/media/fondo.ffb5e563.png"
+  }
+]);
