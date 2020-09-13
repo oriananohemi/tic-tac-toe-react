e01829e8c877f4b3d60752687aa94a5b04d513f1
Oriana <oriana.nohemi22@gmail.com>
2020-09-12 22:02:19 -0500

    cambiar la clase square por una funcion y definir turnos

--- src/index.js
+++ src/index.js
@@ -2,18 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-      className="square" 
-      onClick={()=>{
-        this.props.onClick()
-      }}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -21,13 +15,22 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares})
+  }
   renderSquare(i) {
-    return <Square 
+    return (
+    <Square 
     value={this.state.squares[i]}
     onClick={() => this.handleClick(i)}
-    />;
+    />
+    );
   }
 
   render() {
